ac46d07fa0cb8d28b793b640df6a68a7b13b6ac6
claudiasrn <perezserranoclaudia@gmail.com>
2026-06-04 01:07:50 +0200

    Add basic calculator functions

---
+++ js/calculator.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(dividend, divisor){
+    if(divisor === 0) return "The universe said no";
+    return dividend / divisor;
+}
