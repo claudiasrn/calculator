7b32842b4db5c05443a9eab2707074591be4047f
claudiasrn <perezserranoclaudia@gmail.com>
2026-06-04 01:11:33 +0200

    Add operate function that is called after press =

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,7 @@
+let a = 0;
+let b = 0;
+let operand = "";
+
 function add(a, b){
     return a + b;
 }
@@ -13,4 +17,21 @@ function multiply(a, b){
 function divide(dividend, divisor){
     if(divisor === 0) return "The universe said no";
     return dividend / divisor;
+}
+
+function operate() {
+    switch(operand) {
+        case "+":
+            add(a, b);
+            break;
+        case "-":
+            subtract(a, b);
+            break;
+        case "x":
+            multiply(a, b);
+            break;
+        case "÷":
+            divide(a, b);
+            break;
+    }
 }
